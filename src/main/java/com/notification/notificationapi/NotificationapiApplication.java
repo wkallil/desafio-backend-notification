f5de31ff15e98725437f8a9d382e24225605f02e
Wkallil <kallil.tim@gmail.com>
2024-07-07 16:17:07 -0300

    first commit

---
+++ src/main/java/com/notification/notificationapi/NotificationapiApplication.java
@@ -0,0 +1,13 @@
+package com.notification.notificationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notification/notificationapi/NotificationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.notificationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
